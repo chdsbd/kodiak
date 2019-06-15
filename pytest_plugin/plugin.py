38778eec08626c85bd8cb680072a53d9e57dcaab
Christopher Dignam <chris@dignam.xyz>
2019-06-15 16:35:02 -0400

    .

--- pytest_plugin/plugin.py
+++ pytest_plugin/plugin.py
@@ -7,3 +7,4 @@ import pytest
 def pytest_load_initial_conftests(args, early_config, parser):
     os.environ["SECRET_KEY"] = "some-random-key"
     os.environ["DEBUG"] = "1"
+    os.environ['REDIS_URL'] = ''
