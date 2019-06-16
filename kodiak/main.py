188f2dcc4b54851e65f8fdd188f4ae76b4d457e2
Christopher Dignam <chris@dignam.xyz>
2019-06-15 20:02:47 -0400

    .

--- kodiak/main.py
+++ kodiak/main.py
@@ -10,7 +10,7 @@ import asyncio_redis
 import sentry_sdk
 import structlog
 from asyncio_redis.connection import Connection as RedisConnection
-from asyncio_redis.replies import BlockingPopReply, ZRangeReply
+from asyncio_redis.replies import BlockingZPopReply
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sentry_asgi import SentryMiddleware
@@ -61,7 +61,7 @@ async def repo_queue_consumer(
     log.info("start repo_consumer")
     while True:
         log.info("block for new event")
-        webhook_event_json: ZRangeReply = await connection.bzpopmin([queue_name])
+        webhook_event_json: BlockingZPopReply = await connection.bzpopmin([queue_name])
         webhook_event = WebhookEvent.parse_raw(webhook_event_json.value)
         pull_request = PR(
             owner=webhook_event.repo_owner,
