f904b2bf03697f85e978f2a92c640e57b91af5ca
Christopher Dignam <chris@dignam.xyz>
2019-07-28 11:26:47 -0400

    CR

--- kodiak/test_queries.py
+++ kodiak/test_queries.py
@@ -233,30 +233,33 @@ async def test_get_permissions_for_username_missing(
     assert res == Permission.NONE
 
 
-PERMISSION_OK_READ_USER_RESPONSE = b"""\
-{
-  "permission": "read",
-  "user": {
-    "login": "ghost",
-    "id": 10137,
-    "node_id": "MDQ6VXNlcjEwMTM3",
-    "avatar_url": "https://avatars3.githubusercontent.com/u/10137?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/ghost",
-    "html_url": "https://github.com/ghost",
-    "followers_url": "https://api.github.com/users/ghost/followers",
-    "following_url": "https://api.github.com/users/ghost/following{/other_user}",
-    "gists_url": "https://api.github.com/users/ghost/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/ghost/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/ghost/subscriptions",
-    "organizations_url": "https://api.github.com/users/ghost/orgs",
-    "repos_url": "https://api.github.com/users/ghost/repos",
-    "events_url": "https://api.github.com/users/ghost/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/ghost/received_events",
-    "type": "User",
-    "site_admin": false
-  }
-}"""
+PERMISSION_OK_READ_USER_RESPONSE = (
+    json.dumps(
+        {
+            "permission": "read",
+            "user": {
+                "login": "ghost",
+                "id": 10137,
+                "node_id": "MDQ6VXNlcjEwMTM3",
+                "avatar_url": "https://avatars3.githubusercontent.com/u/10137?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/ghost",
+                "html_url": "https://github.com/ghost",
+                "followers_url": "https://api.github.com/users/ghost/followers",
+                "following_url": "https://api.github.com/users/ghost/following{/other_user}",
+                "gists_url": "https://api.github.com/users/ghost/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/ghost/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/ghost/subscriptions",
+                "organizations_url": "https://api.github.com/users/ghost/orgs",
+                "repos_url": "https://api.github.com/users/ghost/repos",
+                "events_url": "https://api.github.com/users/ghost/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/ghost/received_events",
+                "type": "User",
+                "site_admin": False,
+            },
+        }
+    )
+).encode()
 
 
 @pytest.mark.asyncio
