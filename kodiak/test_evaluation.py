9981addb85f8fe9b470f2e631ceec2d6628b9b2a
Christopher Dignam <chris@dignam.xyz>
2019-12-02 07:49:14 -0500

    fix: travis-ci check compression (#166)
    This is blocked until I can find all the possible status checks that map to `continuous-integration/travis-ci`. I've opened an issue with GitHub, so hopefully they will have information.
    
    fixes #163

--- kodiak/test_evaluation.py
+++ kodiak/test_evaluation.py
@@ -1114,3 +1114,35 @@ def test_requires_commit_signatures(
             valid_signature=False,
             valid_merge_methods=[MergeMethod.squash],
         )
+
+
+def test_partial_branch_protection(
+    pull_request: PullRequest, config: V1, branch_protection: BranchProtectionRule
+) -> None:
+    """
+    In some cases the github api returns partial branch protection contexts
+    """
+    branch_protection.requiredStatusCheckContexts = ["continuous-integration/travis-ci"]
+    branch_protection.requiresApprovingReviews = False
+    pull_request.mergeStateStatus = MergeStateStatus.BLOCKED
+    contexts = [
+        StatusContext(
+            context="continuous-integration/travis-ci/pr", state=StatusState.ERROR
+        ),
+        StatusContext(
+            context="continuous-integration/travis-ci/push", state=StatusState.ERROR
+        ),
+    ]
+    with pytest.raises(NotQueueable, match="failing required status checks"):
+        mergeable(
+            app_id="1234",
+            config=config,
+            pull_request=pull_request,
+            branch_protection=branch_protection,
+            review_requests=[],
+            reviews=[],
+            contexts=contexts,
+            check_runs=[],
+            valid_signature=False,
+            valid_merge_methods=[MergeMethod.squash],
+        )
