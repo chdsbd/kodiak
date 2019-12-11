b4545d34859e7e9c612a5c979dd0ce7dd9934842
Steve Dignam <steve@dignam.xyz>
2019-12-10 20:08:59 -0500

    add: logging for throttle & vendor throttle pkg
    The throttle package we were using was short and sweet but we couldn't
    stick logging into it. Now we vendor it and add a logging option.
    
    Hopefully this will make it more obvious when an installation reaches
    their rate limit.

--- kodiak/throttle.py
+++ kodiak/throttle.py
@@ -1,7 +1,79 @@
 from collections import defaultdict
-from typing import Mapping
+from typing import Mapping, Any, Optional
+import time
+import asyncio
+from collections import deque
+from typing_extensions import Deque
+import structlog
 
-from asyncio_throttle import Throttler
+
+class Throttler:
+    """
+    via https://github.com/hallazzang/asyncio-throttle
+
+    The MIT License (MIT)
+
+    Copyright (c) 2017-2019 Hanjun Kim
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    """
+
+    def __init__(
+        self,
+        rate_limit: float,
+        period: float = 1.0,
+        retry_interval: float = 0.01,
+        log: Optional[structlog.BoundLogger] = None,
+    ) -> None:
+        self.rate_limit = rate_limit
+        self.period = period
+        self.retry_interval = retry_interval
+        self.log = log
+
+        self._task_logs: Deque[float] = deque()
+
+    def flush(self) -> None:
+        now = time.time()
+        while self._task_logs:
+            if now - self._task_logs[0] > self.period:
+                self._task_logs.popleft()
+            else:
+                break
+
+    async def acquire(self) -> None:
+        while True:
+            self.flush()
+            if len(self._task_logs) < self.rate_limit:
+                break
+
+            if self.log is not None:
+                self.log.info("rate limit reached, waiting")
+            await asyncio.sleep(self.retry_interval)
+
+        self._task_logs.append(time.time())
+
+    async def __aenter__(self) -> None:
+        await self.acquire()
+
+    async def __aexit__(self, exc_type: Any, exc: Any, tb: Any) -> None:
+        pass
+
 
 # installation_id => Throttler
 THROTTLER_CACHE: Mapping[str, Throttler] = defaultdict(
@@ -10,5 +82,9 @@ THROTTLER_CACHE: Mapping[str, Throttler] = defaultdict(
 )
 
 
-async def get_thottler_for_installation(*, installation_id: str) -> Throttler:
-    return THROTTLER_CACHE[installation_id]
+def get_thottler_for_installation(
+    *, installation_id: str, log: Optional[structlog.BoundLogger] = None
+) -> Throttler:
+    throttler = THROTTLER_CACHE[installation_id]
+    throttler.log = log
+    return throttler
