92eadc2d5bf4248d03a884db7b06ff360407c35d
Christopher Dignam <chris@dignam.xyz>
2020-01-12 21:45:38 -0500

    Revert merge.optimistic_merge configuration (#229)
    This reverts commit cff7d2cbe641a2619005270e97b884d6d2f4e25e.
    
    This change was broken when testing on staging. A PR that needs an update will get stuck when merge.optimistic_merge is false.
    I'll fix this in another PR, but I want to make master deployable.

--- kodiak/config.py
+++ kodiak/config.py
@@ -65,10 +65,8 @@ class Merge(BaseModel):
     block_on_reviews_requested: bool = False
     # comment on merge conflict and remove automerge label
     notify_on_conflict: bool = True
-    # don't wait for status checks to run before updating branch during merge process
+    # don't wait for status checks to run before updating branch
     optimistic_updates: bool = True
-    # don't wait for status checks to run before queuing PR for merge.
-    optimistic_merge: bool = True
     # configuration for commit message of merge
     message: MergeMessage = MergeMessage()
     # status checks that we don't want to wait to complete when they are in a
