fdad29a4b722ecba49b84fd1484568bdf5a188ba
Christopher Dignam <chris@dignam.xyz>
2020-02-05 19:30:57 -0500

    tests!

---
+++ web_api/core/test_views.py
@@ -0,0 +1,20 @@
+def test_login() -> None:
+    """
+    Login should set the cookie appropriately and allow authenticated requests.
+    """
+    assert False
+
+
+def test_logout() -> None:
+    """
+    Ensure we delete the cookie on logout.
+    The user should no longer be able to access authed routes.
+    """
+    assert False
+
+
+def test_installations() -> None:
+    """
+    Authentication should be restricted to logged in users.
+    """
+    assert False
