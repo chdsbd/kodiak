ea6f7261ca25e9de359d0acf64804a6b83a29527
Christopher Dignam <chris@dignam.xyz>
2020-02-08 10:11:17 -0500

    fmt

--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -11,13 +11,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get("DEBUG")
 
 if DEBUG:
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = "gjzyca8n8$gg^d9u-ts6x5_+=)x*h!=ae&y$%m*ecsjly&*8j3"
 else:
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS: List[str] = ["*"]
 
