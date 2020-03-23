7ebf8952e393088bdfb254ea705c25385be0d1b7
Christopher Dignam <chris@dignam.xyz>
2020-03-22 23:21:41 -0400

    add config for stripe publishable api key

--- web_ui/src/settings.ts
+++ web_ui/src/settings.ts
@@ -19,3 +19,7 @@ export const installUrl = "https://github.com/marketplace/kodiakhq"
 export const docsUrl = "https://kodiakhq.com/docs/quickstart"
 export const helpUrl = "https://kodiakhq.com/help"
 export const loginUrl = `${API_ROOT}/v1/oauth_login`
+
+export const stripePublishableApiKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_API_KEY
+
+console.info(`API_ROOT=${API_ROOT}\nstripePublishableApiKey=${stripePublishableApiKey}`)
