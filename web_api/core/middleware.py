da1cab2b479152f741537daba66ded853ea540e7
Christopher Dignam <chris@dignam.xyz>
2020-03-30 22:37:30 -0400

    add(web_api): /healthz and /readiness endpoints for uptime checking (#332)
    I copied these from recipeyak.
    
    https://github.com/recipeyak/recipeyak/blob/514b372e91dce3756bd120fa0a54bf01a32a5e02/backend/backend/middleware.py#L115-L163

--- web_api/core/middleware.py
+++ web_api/core/middleware.py
@@ -1,12 +1,16 @@
+import enum
+import logging
 from typing import Callable, Optional
 from urllib.parse import urlparse
 
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseServerError, JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 
 from core.auth import get_user
 from core.exceptions import ApiException
+
+logger = logging.getLogger(__name__)
 
 
 class AuthenticationMiddleware(MiddlewareMixin):
@@ -56,3 +60,54 @@ class ExceptionMiddleware(MiddlewareMixin):
         if isinstance(exception, ApiException):
             return JsonResponse(dict(message=exception.message), status=exception.code)
         return None
+
+
+@enum.unique
+class ReadinessError(enum.Enum):
+    PG_BAD_RESPONSE = "PG_BAD_RESPONSE"
+    PG_CANNOT_CONNECT = "PG_CANNOT_CONNECT"
+
+
+class HealthCheckMiddleware:
+    """
+    from: https://www.ianlewis.org/en/kubernetes-health-checks-django
+    """
+
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        if request.method == "GET":
+            if request.path == "/readiness":
+                return self.readiness(request)
+            if request.path == "/healthz":
+                return self.healthz(request)
+        return self.get_response(request)
+
+    def healthz(self, request: HttpRequest) -> HttpResponse:
+        """
+        Note: we don't check the database here because if the database
+        connection failed then this service would restart, not the database.
+        """
+        return HttpResponse("OK")
+
+    def readiness(self, request: HttpRequest) -> HttpResponse:
+        """
+        Connect to each database and do a generic standard SQL query
+        that doesn't write any data and doesn't depend on any tables
+        being present.
+        """
+        try:
+            from django.db import connections  # pylint: disable=import-outside-toplevel
+
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute("SELECT 1;")
+                row = cursor.fetchone()
+                if row is None:
+                    return HttpResponseServerError(ReadinessError.PG_BAD_RESPONSE)
+        except Exception:  # pylint: disable=broad-except
+            logger.exception("could not connect to postgres")
+            return HttpResponseServerError(ReadinessError.PG_CANNOT_CONNECT)
+
+        return HttpResponse("OK")
