2dd9244e8915f804cf0532080e5f69a6b552ca6c
Steve Dignam <steve@dignam.xyz>
2020-05-10 18:17:00 -0400

    ref to useTeamApi hook

--- web_ui/src/components/SubscriptionAlert.tsx
+++ web_ui/src/components/SubscriptionAlert.tsx
@@ -1,50 +1,8 @@
 import React from "react"
-import { Link, useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { Alert } from "react-bootstrap"
 import { Current } from "src/world"
-
-type State<T> =
-  | { readonly status: "loading" }
-  | { readonly status: "failure" }
-  | {
-      readonly status: "success"
-      readonly data: T
-    }
-
-type Subscription =
-  | {
-      // personal user, subscription valid, or trial account
-      readonly type: "VALID_SUBSCRIPTION"
-    }
-  | { readonly type: "TRIAL_EXPIRED" }
-  | { readonly type: "SUBSCRIPTION_EXPIRED" }
-  | {
-      readonly type: "SUBSCRIPTION_OVERAGE"
-      readonly activeUserCount: number
-
-      readonly licenseCount: number
-    }
-
-function useSubscriptionInfo({ teamId }: { readonly teamId: string }) {
-  const [state, setState] = React.useState<State<Subscription>>({
-    status: "loading",
-  })
-  React.useEffect(() => {
-    setState({ status: "loading" })
-    Current.api
-      .getSubscriptionInfo({ teamId })
-      .then(res => {
-        setState({
-          status: "success",
-          data: res,
-        })
-      })
-      .catch(() => {
-        setState({ status: "failure" })
-      })
-  }, [setState, teamId])
-  return state
-}
+import { useTeamApi } from "src/useApi"
 
 function TrialExpiredAlert() {
   return (
@@ -85,8 +43,7 @@ function SubscriptionExceededAlert({
 }
 
 export function SubscriptionAlert() {
-  const teamId = useParams<{ readonly team_id: string }>().team_id
-  const state = useSubscriptionInfo({ teamId })
+  const state = useTeamApi(Current.api.getSubscriptionInfo)
 
   if (state.status === "loading" || state.status === "failure") {
     return null
