75614b06cd538edaedd4a0c133793456b7b5392c
Christopher Dignam <chris@dignam.xyz>
2020-05-16 10:53:28 -0400

    test_mergeable_missing_push_allowance

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -616,43 +616,18 @@ async def test_mergeable_missing_branch_protection() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_missing_push_allowance(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_missing_push_allowance() -> None:
     """
     We should warn when user is missing a push allowance with restrictsPushes
     enabled. If Kodiak isn't given an allowance it won't be able to merge pull
     requests and will get a mysterious "merge blocked by GitHub requirements".
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    branch_protection = create_branch_protection()
     branch_protection.restrictsPushes = True
     branch_protection.pushAllowances = NodeListPushAllowance(nodes=[])
-    await mergeable(
-        api=api,
-        config=config,
-        config_str=config_str,
-        config_path=config_path,
-        pull_request=pull_request,
-        branch_protection=branch_protection,
-        review_requests=[],
-        reviews=[review],
-        contexts=[context],
-        check_runs=[check_run],
-        valid_signature=False,
-        valid_merge_methods=[MergeMethod.squash],
-        merging=False,
-        is_active_merge=False,
-        skippable_check_timeout=5,
-        api_call_retry_timeout=5,
-        api_call_retry_method_name=None,
-    )
+    await mergeable(api=api, branch_protection=branch_protection)
     assert api.set_status.call_count == 1
     assert api.dequeue.call_count == 1
     assert "config error" in api.set_status.calls[0]["msg"]
