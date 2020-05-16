fe597c9d7e8e21ec57b907057d56cfab8c49ba01
Christopher Dignam <chris@dignam.xyz>
2020-05-16 11:39:30 -0400

    test_mergeable_blacklist_title_regex

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -822,42 +822,19 @@ async def test_mergeable_has_blacklist_labels() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_blacklist_title_regex(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_blacklist_title_regex() -> None:
     """
     block merge if title regex matches pull request
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    config = create_config()
+    pull_request = create_pull_request()
+
     pull_request.title = "WIP: add new feature"
     config.merge.blacklist_title_regex = "^WIP.*"
 
-    await mergeable(
-        api=api,
-        config=config,
-        config_str=config_str,
-        config_path=config_path,
-        pull_request=pull_request,
-        branch_protection=branch_protection,
-        review_requests=[],
-        reviews=[review],
-        contexts=[context],
-        check_runs=[check_run],
-        valid_signature=False,
-        valid_merge_methods=[MergeMethod.squash],
-        merging=False,
-        is_active_merge=False,
-        skippable_check_timeout=5,
-        api_call_retry_timeout=5,
-        api_call_retry_method_name=None,
-    )
+    await mergeable(api=api, config=config, pull_request=pull_request)
     assert api.set_status.call_count == 1
     assert api.dequeue.call_count == 1
     assert "cannot merge" in api.set_status.calls[0]["msg"]
