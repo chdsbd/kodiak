869f4c9a7f5efdc464b717fd173a24f8c11085c8
Christopher Dignam <chris@dignam.xyz>
2020-05-16 17:39:22 -0400

    test_mergeable_pull_request_need_test_commit_merging

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -1293,44 +1293,19 @@ async def test_mergeable_pull_request_need_test_commit() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_pull_request_need_test_commit_merging(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_pull_request_need_test_commit_merging() -> None:
     """
     If we're merging a PR we should raise the PollForever exception instead of
     returning. This way we stay in the merge loop.
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    pull_request = create_pull_request()
+
     pull_request.mergeable = MergeableState.UNKNOWN
 
     with pytest.raises(PollForever):
-        await mergeable(
-            api=api,
-            config=config,
-            config_str=config_str,
-            config_path=config_path,
-            pull_request=pull_request,
-            branch_protection=branch_protection,
-            review_requests=[],
-            reviews=[review],
-            contexts=[context],
-            check_runs=[check_run],
-            valid_signature=False,
-            valid_merge_methods=[MergeMethod.squash],
-            is_active_merge=False,
-            skippable_check_timeout=5,
-            api_call_retry_timeout=5,
-            api_call_retry_method_name=None,
-            #
-            merging=True,
-        )
+        await mergeable(api=api, pull_request=pull_request, merging=True)
     assert api.set_status.call_count == 0
     assert api.dequeue.call_count == 0
     assert api.trigger_test_commit.call_count == 1
