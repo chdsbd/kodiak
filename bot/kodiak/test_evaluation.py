67dd06f1fcf7fa18732ff2b9e33ec87ac4776d5f
Christopher Dignam <chris@dignam.xyz>
2020-05-16 17:13:39 -0400

    test_mergeable_pull_request_merged_delete_branch

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -985,45 +985,22 @@ async def test_mergeable_pull_request_merged_no_delete_branch() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_pull_request_merged_delete_branch(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_pull_request_merged_delete_branch() -> None:
     """
     if a PR is already merged we shouldn't take anymore action on it besides
     deleting the branch if configured.
 
     Here we test with the delete_branch_on_merge config enabled.
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    config = create_config()
+    pull_request = create_pull_request()
+
     pull_request.state = PullRequestState.MERGED
     config.merge.delete_branch_on_merge = True
 
-    await mergeable(
-        api=api,
-        config=config,
-        config_str=config_str,
-        config_path=config_path,
-        pull_request=pull_request,
-        branch_protection=branch_protection,
-        review_requests=[],
-        reviews=[review],
-        contexts=[context],
-        check_runs=[check_run],
-        valid_signature=False,
-        valid_merge_methods=[MergeMethod.squash],
-        merging=False,
-        is_active_merge=False,
-        skippable_check_timeout=5,
-        api_call_retry_timeout=5,
-        api_call_retry_method_name=None,
-    )
+    await mergeable(api=api, config=config, pull_request=pull_request)
     assert api.set_status.call_count == 0
     assert api.dequeue.call_count == 1
     assert api.delete_branch.call_count == 1
