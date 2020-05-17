cedafcbe6f51c9eb6e1a3143d8f6566ae6293568
Christopher Dignam <chris@dignam.xyz>
2020-05-16 23:09:29 -0400

    test_mergeable_update_branch_immediately_mode_merging

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -1816,20 +1816,16 @@ async def test_mergeable_update_branch_immediately() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_update_branch_immediately_mode_merging(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_update_branch_immediately_mode_merging() -> None:
     """
     update branch immediately if configured. When we are merging we should raise the PollForever exception to keep the merge loop going instead of returning.
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    pull_request = create_pull_request()
+    branch_protection = create_branch_protection()
+    config = create_config()
+
     pull_request.mergeStateStatus = MergeStateStatus.BEHIND
     branch_protection.requiresStrictStatusChecks = True
     config.merge.update_branch_immediately = True
@@ -1838,21 +1834,8 @@ async def test_mergeable_update_branch_immediately_mode_merging(
         await mergeable(
             api=api,
             config=config,
-            config_str=config_str,
-            config_path=config_path,
             pull_request=pull_request,
             branch_protection=branch_protection,
-            review_requests=[],
-            reviews=[review],
-            check_runs=[check_run],
-            contexts=[context],
-            valid_signature=False,
-            valid_merge_methods=[MergeMethod.squash],
-            is_active_merge=False,
-            skippable_check_timeout=5,
-            api_call_retry_timeout=5,
-            api_call_retry_method_name=None,
-            #
             merging=True,
         )
     assert api.set_status.call_count == 1
