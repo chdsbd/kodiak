52d4f97d469a6f43b8527c44a0424bea33e48b43
Christopher Dignam <chris@dignam.xyz>
2020-05-16 23:12:02 -0400

    test_mergeable_queue_in_progress

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -1931,20 +1931,17 @@ async def test_mergeable_need_branch_update() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_queue_in_progress(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_queue_in_progress() -> None:
     """
     If a PR has pending status checks or is behind, we still consider it eligible for merge and throw it in the merge queue.
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    pull_request = create_pull_request()
+    branch_protection = create_branch_protection()
+    config = create_config()
+    context = create_context()
+
     config.merge.optimistic_updates = False
     pull_request.mergeStateStatus = MergeStateStatus.BEHIND
     branch_protection.requiresStrictStatusChecks = True
@@ -1957,21 +1954,9 @@ async def test_mergeable_queue_in_progress(
     await mergeable(
         api=api,
         config=config,
-        config_str=config_str,
-        config_path=config_path,
         pull_request=pull_request,
         branch_protection=branch_protection,
-        review_requests=[],
-        reviews=[review],
-        check_runs=[check_run],
         contexts=[context],
-        valid_signature=False,
-        valid_merge_methods=[MergeMethod.squash],
-        merging=False,
-        is_active_merge=False,
-        skippable_check_timeout=5,
-        api_call_retry_timeout=5,
-        api_call_retry_method_name=None,
     )
     assert api.set_status.call_count == 1
     assert "enqueued for merge" in api.set_status.calls[0]["msg"]
