e9ae5b03f4a47121b0dd37d9a947d06178d716cc
Christopher Dignam <chris@dignam.xyz>
2020-05-16 21:20:51 -0400

    test_mergeable_skippable_contexts_with_status_check

--- bot/kodiak/test_evaluation.py
+++ bot/kodiak/test_evaluation.py
@@ -1612,20 +1612,18 @@ async def test_mergeable_travis_ci_checks_success() -> None:
 
 
 @pytest.mark.asyncio
-async def test_mergeable_skippable_contexts_with_status_check(
-    api: MockPrApi,
-    config: V1,
-    config_path: str,
-    config_str: str,
-    pull_request: PullRequest,
-    branch_protection: BranchProtectionRule,
-    review: PRReview,
-    context: StatusContext,
-    check_run: CheckRun,
-) -> None:
+async def test_mergeable_skippable_contexts_with_status_check() -> None:
     """
     If a skippable check hasn't finished, we shouldn't do anything.
     """
+    api = create_api()
+    mergeable = create_mergeable()
+    pull_request = create_pull_request()
+    branch_protection = create_branch_protection()
+    config = create_config()
+    context = create_context()
+    check_run = create_check_run()
+
     pull_request.mergeStateStatus = MergeStateStatus.BLOCKED
     branch_protection.requiresStatusChecks = True
     branch_protection.requiredStatusCheckContexts = ["WIP", "ci/test-api"]
@@ -1638,21 +1636,10 @@ async def test_mergeable_skippable_contexts_with_status_check(
     await mergeable(
         api=api,
         config=config,
-        config_str=config_str,
-        config_path=config_path,
         pull_request=pull_request,
         branch_protection=branch_protection,
-        review_requests=[],
-        reviews=[review],
         check_runs=[check_run],
         contexts=[context],
-        valid_signature=False,
-        valid_merge_methods=[MergeMethod.squash],
-        merging=False,
-        is_active_merge=False,
-        skippable_check_timeout=5,
-        api_call_retry_timeout=5,
-        api_call_retry_method_name=None,
     )
     assert api.set_status.call_count == 1
     assert api.dequeue.call_count == 0
