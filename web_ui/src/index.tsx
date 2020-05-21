3d278450f68b2ed2fff30f3cda561d095bd89e75
Christopher Dignam <chris@dignam.xyz>
2020-05-20 21:00:58 -0400

    sentry-browser

--- web_ui/src/index.tsx
+++ web_ui/src/index.tsx
@@ -2,5 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./custom.scss"
 import App from "./components/App"
+import * as settings from "./settings"
+
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({ dsn: settings.SENTRY_DSN, release: settings.RELEASE });
 
 ReactDOM.render(<App />, document.getElementById("root"))
--- web_ui/src/settings.ts
+++ web_ui/src/settings.ts
@@ -15,6 +15,8 @@ function getApiRoot(): string {
   return "https://api.kodiakhq.com"
 }
 export const API_ROOT = getApiRoot() || "https://api.kodiakhq.com"
+export const RELEASE = process.env.REACT_APP_KODIAK_RELEASE
+export const SENTRY_DSN = 'https://0012ad6693d042d1b57ac5f00918b3bd@o64108.ingest.sentry.io/3352104'
 export const installUrl = "https://github.com/marketplace/kodiakhq"
 export const docsUrl = "https://kodiakhq.com/docs/quickstart"
 export const helpUrl = "https://kodiakhq.com/help"
