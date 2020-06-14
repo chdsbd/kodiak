ad5e61e32854db7970cb761d3b4d25b8a6b2de22
Christopher Dignam <chris@dignam.xyz>
2020-06-14 15:37:33 -0400

    update(web_ui): copy for subscription benefits (#433)
    Co-authored-by: Steve Dignam <7340772+sbdchd@users.noreply.github.com>

--- web_ui/src/components/UsageBillingPage.tsx
+++ web_ui/src/components/UsageBillingPage.tsx
@@ -894,7 +894,8 @@ function BillingDocumentation({
               </li>
               <li>priority support – get priority help configuring Kodiak</li>
               <li>
-                sustain Kodiak – help us cover server costs and support Kodiak
+                sustain Kodiak – help us cover server costs and feature
+                development
               </li>
             </ul>
           </Col>
