2a82c909245598fcbca067817fd4a0e40ce1b349
Christopher Dignam <chris@dignam.xyz>
2020-06-21 15:50:51 -0400

    lint

--- web_api/core/test_views.py
+++ web_api/core/test_views.py
@@ -353,7 +353,7 @@ def test_usage_limit_billing_access_to_owners_member(
     authed_client: Client, user: User, other_user: User, mocker: Any
 ) -> None:
     account, membership = create_org_account(user=user, role="member")
-    stripe_customer_information = create_stripe_customer_info(
+    create_stripe_customer_info(
         customer_id=account.stripe_customer_id
     )
     res = authed_client.get(f"/v1/t/{account.id}/usage_billing")
@@ -1052,7 +1052,7 @@ def test_update_stripe_customer_info_permission(
     authed_client: Client, user: User, patch_stripe_customer_modify: object
 ) -> None:
     account, membership = create_org_account(user, role="member")
-    stripe_customer_info = create_stripe_customer_info(
+    create_stripe_customer_info(
         customer_id=account.stripe_customer_id
     )
 
