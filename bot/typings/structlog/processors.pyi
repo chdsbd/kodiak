0357969c67d64f5685670836b6bcaccb03d0b220
Steve Dignam <steve@dignam.xyz>
2020-12-27 14:46:45 -0500

    add(bot): type stubs for structlog
    Not sure why, but never got around to structlog when I previously stubbed
    out the dependencies missing types.
    
    Structlog now supports types, but there hasn't been a release with that
    functionality yet.

---
+++ bot/typings/structlog/processors.pyi
@@ -0,0 +1,46 @@
+from typing import Any, Callable, Dict, Optional, Sequence
+
+_EventDict = Dict[str, Any]
+
+class KeyValueRenderer:
+    def __init__(
+        self,
+        sort_keys: bool = ...,
+        key_order: Optional[Sequence[str]] = ...,
+        drop_missing: bool = ...,
+        repr_native_str: bool = ...,
+    ) -> None: ...
+    def __call__(self, _: Any, __: Any, event_dict: _EventDict) -> Any: ...
+
+class UnicodeEncoder:
+    def __init__(self, encoding: str = ..., errors: str = ...) -> None: ...
+    def __call__(
+        self, logger: Any, name: str, event_dict: _EventDict
+    ) -> _EventDict: ...
+
+class UnicodeDecoder:
+    def __init__(self, encoding: str = ..., errors: str = ...) -> None: ...
+    def __call__(
+        self, logger: Any, name: str, event_dict: _EventDict
+    ) -> _EventDict: ...
+
+class JSONRenderer:
+    def __init__(
+        self, serializer: Callable[..., str] = ..., **dumps_kw: Any
+    ) -> None: ...
+    def __call__(self, logger: Any, name: str, event_dict: _EventDict) -> str: ...
+
+def format_exc_info(logger: Any, name: str, event_dict: _EventDict) -> _EventDict: ...
+
+class TimeStamper: ...
+
+class ExceptionPrettyPrinter:
+    def __init__(self, file: Optional[Any] = ...) -> None: ...
+    def __call__(
+        self, logger: Any, name: str, event_dict: _EventDict
+    ) -> _EventDict: ...
+
+class StackInfoRenderer:
+    def __call__(
+        self, logger: Any, name: str, event_dict: _EventDict
+    ) -> _EventDict: ...
