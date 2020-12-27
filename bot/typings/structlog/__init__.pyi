cfde1c97c61276be6912aa2882456d2049365118
Steve Dignam <steve@dignam.xyz>
2020-12-27 15:05:58 -0500

    add(bot): type stubs for structlog (#600)
    Not sure why, but never got around to structlog when I previously stubbed
    out the dependencies missing types.
    
    Structlog now supports types, but there hasn't been a release with that
    functionality yet.

---
+++ bot/typings/structlog/__init__.pyi
@@ -0,0 +1,6 @@
+from structlog import processors as processors  # noqa: F401
+from structlog import stdlib as stdlib  # noqa: F401
+from structlog._config import configure as configure  # noqa: F401
+from structlog._config import get_logger as get_logger  # noqa: F401
+from structlog._config import reset_defaults as reset_defaults  # noqa: F401
+from structlog.stdlib import BoundLogger as BoundLogger  # noqa: F401
