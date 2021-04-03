c04716e624ef70602c6e1e93f29d9a6af3b2ab50
Christopher Dignam <chris@dignam.xyz>
2021-04-03 17:17:00 -0400

    ref(bot): improve cli startup time

--- bot/kodiak/cli.py
+++ bot/kodiak/cli.py
@@ -1,13 +1,6 @@
-import asyncio
-from pathlib import Path
 from typing import Any, Dict, List
 
 import click
-import requests
-
-from kodiak import app_config as conf
-from kodiak.config import V1
-from kodiak.queries import generate_jwt, get_token_for_install
 
 
 @click.group()
@@ -17,6 +10,9 @@ def cli() -> None:
 
 @cli.command(help="create a JWT for testing GitHub API endpoints")
 def create_jwt() -> None:
+    from kodiak.queries import generate_jwt
+    from kodiak import app_config as conf
+
     click.echo(
         generate_jwt(private_key=conf.PRIVATE_KEY, app_identifier=conf.GITHUB_APP_ID)
     )
@@ -24,11 +20,17 @@ def create_jwt() -> None:
 
 @cli.command(help="generate the JSON schema for the .kodiak.toml")
 def gen_conf_json_schema() -> None:
+    from kodiak.config import V1
+
     click.echo(V1.schema_json(indent=2))
 
 
 @cli.command(help="list all installs for the Kodiak GitHub App")
 def list_installs() -> None:
+    from kodiak.queries import generate_jwt
+    from kodiak import app_config as conf
+    import requests
+
     app_token = generate_jwt(
         private_key=conf.PRIVATE_KEY, app_identifier=conf.GITHUB_APP_ID
     )
@@ -63,6 +65,9 @@ def token_for_install(install_id: str) -> None:
     outputs the OAuth token for a given installation id.
     This is useful to help debug installation problems
     """
+    import asyncio
+    from kodiak.queries import get_token_for_install
+
     token = asyncio.run(get_token_for_install(installation_id=install_id))
     click.echo(token)
 
@@ -73,7 +78,11 @@ def validate_config(config_path: str) -> None:
     """
     parse and output the json representation of a Kodiak config
     """
+    from pathlib import Path
+
     cfg_text = Path(config_path).read_text()
+    from kodiak.config import V1
+
     cfg_file = V1.parse_toml(cfg_text)
     assert isinstance(cfg_file, V1)
     click.echo(cfg_file.json(indent=2))
