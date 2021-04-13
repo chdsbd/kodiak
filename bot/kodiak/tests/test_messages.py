618993d7db1bf27847d9e938ccef28b084b8a13c
Steve Dignam <steve@dignam.xyz>
2021-04-13 19:40:12 -0400

    deps(api,bot): bump flake8-pie to latest (#650)
    Includes some new rules
    
    https://github.com/sbdchd/flake8-pie

--- bot/kodiak/tests/test_messages.py
+++ bot/kodiak/tests/test_messages.py
@@ -1,14 +1,16 @@
 from kodiak.messages import get_markdown_for_api_call_errors
+from kodiak.pull_request import APICallError
 
 
 def test_get_markdown_for_api_call_errors() -> None:
-    class APICallRetry:
-        api_name = "pull_request/merge"
-        http_status = "405"
-        response_body = '{"message":"This branch must not contain merge commits.","documentation_url":"https://docs.github.com/articles/about-protected-branches”}'
+    api_call_retry = APICallError(
+        api_name="pull_request/merge",
+        http_status="405",
+        response_body='{"message":"This branch must not contain merge commits.","documentation_url":"https://docs.github.com/articles/about-protected-branches”}',
+    )
 
     assert (
-        get_markdown_for_api_call_errors(errors=[APICallRetry(), APICallRetry()])
+        get_markdown_for_api_call_errors(errors=[api_call_retry, api_call_retry])
         == """\
 Errors encountered when contacting GitHub API.
 
