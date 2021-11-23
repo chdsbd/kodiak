ee79ca27d13d4ccca5d6a80603bf1916650bde5a
Christopher Dignam <chris@dignam.xyz>
2021-11-23 12:15:36 -0500

    support Python 3.8 in tests
    I upgraded my local Python version to 3.8 and some of the tests were failing. Removing the future wrapping fixes the issue.

--- bot/kodiak/test_utils.py
+++ bot/kodiak/test_utils.py
@@ -1,10 +1,20 @@
 import asyncio
+import sys
 from typing import TypeVar
 
 T = TypeVar("T")
 
+# some change happened in Python 3.8 to eliminate the need for wrapping mock
+# results.
+if sys.version_info < (3, 8):
 
-def wrap_future(x: T) -> "asyncio.Future[T]":
-    fut: "asyncio.Future[T]" = asyncio.Future()
-    fut.set_result(x)
-    return fut
+    def wrap_future(x: T) -> "asyncio.Future[T]":
+        fut: "asyncio.Future[T]" = asyncio.Future()
+        fut.set_result(x)
+        return fut
+
+
+else:
+
+    def wrap_future(x: T) -> "T":
+        return x
