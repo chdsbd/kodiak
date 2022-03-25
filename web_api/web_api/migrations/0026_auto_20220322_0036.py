20dd383c717146f7b7beeb588ac3df735bc6da50
Steve Dignam <steve@dignam.xyz>
2022-03-25 19:37:37 -0400

    fmt

--- web_api/web_api/migrations/0026_auto_20220322_0036.py
+++ web_api/web_api/migrations/0026_auto_20220322_0036.py
@@ -6,48 +6,48 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('web_api', '0025_auto_20200902_0052'),
+        ("web_api", "0025_auto_20200902_0052"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='account',
-            name='created_at',
+            model_name="account",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='accountmembership',
-            name='created_at',
+            model_name="accountmembership",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='githubevent',
-            name='created_at',
+            model_name="githubevent",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='pullrequestactivity',
-            name='created_at',
+            model_name="pullrequestactivity",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='pullrequestactivityprogress',
-            name='created_at',
+            model_name="pullrequestactivityprogress",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='created_at',
+            model_name="user",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='userpullrequestactivity',
-            name='created_at',
+            model_name="userpullrequestactivity",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
         migrations.AlterField(
-            model_name='userpullrequestactivityprogress',
-            name='created_at',
+            model_name="userpullrequestactivityprogress",
+            name="created_at",
             field=models.DateTimeField(auto_now_add=True, db_index=True),
         ),
     ]
