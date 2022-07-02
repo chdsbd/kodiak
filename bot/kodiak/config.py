17ef952935ac4dc1b92b7477291ec84ddc714909
Christopher Dignam <chris@dignam.xyz>
2022-07-02 19:58:22 -0400

    bump pydantic and add types

--- bot/kodiak/config.py
+++ bot/kodiak/config.py
@@ -172,6 +172,6 @@ class V1(BaseModel):
         cls, content: str
     ) -> Union[V1, toml.TomlDecodeError, ValidationError]:
         try:
-            return cls.parse_obj(cast(Dict[str, Any], toml.loads(content)))
+            return cls.parse_obj(toml.loads(content))
         except (toml.TomlDecodeError, ValidationError) as e:
             return e
