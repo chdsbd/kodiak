8d91db0fd9d7fd84d23925e4154d9557e2290396
Christopher Dignam <chris@dignam.xyz>
2022-07-02 20:04:36 -0400

    fmt

--- bot/typings/jwt.pyi
+++ bot/typings/jwt.pyi
@@ -1,2 +1,5 @@
 from typing_extensions import Literal
-def encode(*, payload: dict[str, object], key: str, algorithm: Literal["RS256"]) -> bytes: ...
+
+def encode(
+    *, payload: dict[str, object], key: str, algorithm: Literal["RS256"]
+) -> bytes: ...
