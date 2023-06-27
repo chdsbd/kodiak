8386ea83c027620100324b6be7a28fc404d9e1c0
Christopher Dignam <chris@dignam.xyz>
2023-06-26 21:05:38 -0400

    wip

--- bot/kodiak/queue.py
+++ bot/kodiak/queue.py
@@ -479,17 +479,11 @@ class RedisWebhookQueue:
             await self.start_webhook_worker(queue_name=queue_name)
 
     async def start_webhook_worker(self, *, queue_name: str) -> None:
-        concurrency_str = await redis_bot.get("queue_concurrency:" + queue_name)
-        try:
-            concurrency = int(concurrency_str or 1)
-        except ValueError:
-            concurrency = 1
-
         self._start_worker(
             queue_name,
             "webhook",
             webhook_event_consumer(webhook_queue=self, queue_name=queue_name),
-            concurrency=concurrency,
+            concurrency=conf.WEBHOOK_WORKER_CONCURRENCY.get(queue_name) or 1,
         )
 
     def start_repo_worker(self, *, queue_name: str) -> None:
@@ -533,22 +527,11 @@ class RedisWebhookQueue:
         tasks_to_create = concurrency - len(new_workers)
 
         tasks_created = 0
-        tasks_cancelled = 0
         # we need to create tasks
         if tasks_to_create > 0:
             for _ in range(tasks_to_create):
                 new_workers.append((asyncio.create_task(fut), kind))
                 tasks_created += 1
-        # we need to remove tasks
-        elif tasks_to_create < 0:
-            # split off tasks we need to cancel.
-            new_workers, workers_to_delete = (
-                new_workers[:concurrency],
-                new_workers[concurrency:],
-            )
-            for (task, _kind) in workers_to_delete:
-                task.cancel()
-                tasks_cancelled += 1
 
         self.worker_tasks[key] = new_workers
         log.info(
@@ -556,7 +539,6 @@ class RedisWebhookQueue:
             previous_task_count=previous_task_count,
             failed_task_count=failed_task_count,
             tasks_created=tasks_created,
-            tasks_cancelled=tasks_cancelled,
         )
 
     async def enqueue(self, *, event: WebhookEvent) -> None:
